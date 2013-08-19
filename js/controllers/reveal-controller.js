163b6dfd8188f2f9cf1c992779290d3e5135b2ac
Ryan Ryan <ryan.anklam@gmail.com>
2013-08-19 07:16:47 -0500

    added reveal controller

---
+++ js/controllers/reveal-controller.js
@@ -0,0 +1,1 @@
+Mello.RevealController = Ember.ObjectController.extend();
--- js/routes/lists-route.js
+++ js/routes/lists-route.js
@@ -1,14 +1,21 @@
 Mello.ListsRoute = Ember.Route.extend({
-  model: function() {
-   	return [
-   		{
-   			id : 1,
-   			name : 'ToDo'
-   		},
-   		{
-   			id : 2,
-   			name : 'Doing'
-   		}		
-   	]
-  }
+	model: function() {
+		return [
+			{
+				id : 1,
+				name : 'ToDo'
+			},
+			{
+				id : 2,
+				name : 'Doing'
+			}
+		];
+	},
+	events : {
+	   openModal : function( content ){
+	       this.controllerFor( 'reveal' ).set( 'content', content );
+	       
+	       $('#myModal').foundation('reveal', 'open');
+	   }
+	}
 });
