1d404d1ac6293b6b0bdca68d6b1bbe12318fe6a8
Ryan Ryan <ryan.anklam@gmail.com>
2013-08-26 22:25:38 -0500

    got saving editing cards to work in a custom view

--- js/views/edit-card-view.js
+++ js/views/edit-card-view.js
@@ -3,16 +3,17 @@ App.EditCardView = Ember.View.extend({
 	templateName : 'editCardView',
 
 	keys : {
-		escape : 27
-	},
-
-	insertNewline : function(){
-		this.toggleController( false );
+		escape : 27,
+		enter : 13
 	},
 
 	keyUp: function( e ){
-		if( e.which === this.keys.escape ){
+		if( e.which === this.keys.escape || e.which === this.keys.enter ){
 			this.toggleController( false );
+
+			if( e.which === this.keys.enter){
+				this.get( 'controller.model' ).save();
+			}
 		}
 	},
 
