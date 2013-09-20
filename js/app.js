18abe31a20073e2f52b79aa8ae379cfa74161adb
Ryan Anklam <ryan.anklam@gmail.com>
2013-09-19 19:31:05 -0700

    remove search

--- js/app.js
+++ js/app.js
@@ -1,12 +1,8 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-	this.resource( 'lists', { path : '/' }, function(){
-		this.route( 'search', { path: '/lists/search/:query' } );
-	});
-
+	this.resource( 'lists', { path : '/' } );
 	this.resource( 'about' );
-
 });
 
 App.query = '';
